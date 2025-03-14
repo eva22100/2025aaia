edabc8329579b88ddde22210ac6eeff334014eea
青空 <128980708+eva22100@users.noreply.github.com>
2025-03-14 12:07:53 +0800

    week04

---
+++ week04/week04-2.py
@@ -0,0 +1,7 @@
+#week04-2.py
+
+N=int(input())
+
+for i in range(1,N+1,1):
+	if i%2==0:
+		print(i,end=" ")
---
+++ week04/week04-3.cpp
@@ -0,0 +1,20 @@
+///week04-3.cpp
+#include <stdio.h>
+int main()
+{
+    int N=5;
+    for(int i=0;i<N;i++){
+        printf("%d ",i);
+    }
+    printf("\n");
+
+    for(int i=1;i<N;i++){
+        printf("%d ",i);
+    }
+    printf("\n");
+    for(int i=1;i<N;i--){
+        printf("%d ",i);
+    }
+    printf("\n");
+
+}
