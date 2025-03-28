4f13cc8dfd88dc02b64086c9246d0126c6755521
青空 <128980708+eva22100@users.noreply.github.com>
2025-03-28 12:01:12 +0800

    week06

---
+++ week06/week06-2.cpp
@@ -0,0 +1,11 @@
+///week06-2.cpp
+#include <stdio.h>
+#include <math
+int main()
+{
+    if(2) printf("2 good 成立\n");
+    if(1) printf("1 good 成立\n");
+    if(0) printf("0 有成立嗎?\n");
+    if(-1) printf("-1 good 成立\n");
+    while(1) printf("1 good 成立\n");
+}
